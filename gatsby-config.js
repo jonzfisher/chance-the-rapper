82b74f331e6b191ee2ab1144955ad14abb8bd743
Jonathan Fisher <jfisher@eab.com>
2018-01-24 08:52:17 -0600

    add gh-pages support to the site

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,15 +2,6 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `src/songs/`,
-        name: "markdown-pages",
-      },
-    },
-    `gatsby-transformer-remark`,
-  ],
+  plugins: ['gatsby-plugin-react-helmet'],
+  pathPrefix: "/chance-the-rapper",
 };
