83a179098d89830385af04eaa3ad8a6dfe79b2eb
Jonathan Fisher <jfisher@eab.com>
2018-01-31 12:39:12 -0600

    songs page component

--- src/pages/songs.js
+++ src/pages/songs.js
@@ -4,43 +4,47 @@ import Link from 'gatsby-link'
 import './songs.css'
 import chance from './chance_profile.png'
 
-const SongsPage = () => (
-  <div style={{
-    height: '100%',
-  }}>
-    <Link style={{
-      textAlign: 'center',
-      display: 'block',
-    }} to="/">
-      Home
-    </Link>
-    <h1 style={{
-      textAlign: 'center',
-      color: 'white',
-    }}>Songs</h1>
-    <ul style={{
-      textAlign: 'center',
-      marginTop: '2rem',
+const SongsPage = () => {
+
+  
+  return (
+    <div style={{
+      height: '100%',
     }}>
-      <li>
-        <Link className="songs-link" to="/songs/all-we-got">All We Got</Link>
-      </li>
-      <li>
-        <Link className="songs-link" to="/songs/blessings">Blessings</Link>
-      </li>
-      <li>
-        <Link className="songs-link" to="/songs/same-drugs">Same Drugs</Link>
-      </li>
-      <li>
-        <Link className="songs-link" to="/songs/finish-line-drown">Finish Line / Drown</Link>
-      </li>
-    </ul>
-    <img src={ chance } style={{
-      position: 'absolute',
-      bottom: 0,
-      margin: '0 0 0 8rem',
-    }}/>
-  </div>
-)
+      <Link style={{
+        textAlign: 'center',
+        display: 'block',
+      }} to="/">
+        Home
+      </Link>
+      <h1 style={{
+        textAlign: 'center',
+        color: 'white',
+      }}>Songs</h1>
+      <ul style={{
+        textAlign: 'center',
+        marginTop: '2rem',
+      }}>
+        <li>
+          <Link className="songs-link" to="/songs/all-we-got">All We Got</Link>
+        </li>
+        <li>
+          <Link className="songs-link" to="/songs/blessings">Blessings</Link>
+        </li>
+        <li>
+          <Link className="songs-link" to="/songs/same-drugs">Same Drugs</Link>
+        </li>
+        <li>
+          <Link className="songs-link" to="/songs/finish-line-drown">Finish Line / Drown</Link>
+        </li>
+      </ul>
+      <img src={ chance } style={{
+        position: 'absolute',
+        bottom: 0,
+        margin: '0 0 0 8rem',
+      }}/>
+    </div>
+  )
+}
 
 export default SongsPage
